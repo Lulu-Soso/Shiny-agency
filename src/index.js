ac2485c7c990bbf4d60a3ff1f36e4747a7ef7380
Lulu-Soso <lucas.sokha@gmail.com>
2023-05-23 10:24:02 +0200

    create first components Home Header Error

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Survey from './pages/Survey'
+import Header from './components/Header'
+import Error from './components/Error'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/" element={<Survey />} />
+        <Route path="/survey/:questionNumber" element={<Survey />} />
+        <Route path="/*" element={<Error />} />
       </Routes>
     </Router>
   </React.StrictMode>,
